2f9c19c4e7f8e67b3348c23e391b1733745f6cda
Pedro Alencar <pedromariutti@gmail.com>
2023-03-10 17:59:22 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const url = 'https://jsonplaceholder.typicode.com/posts?_page=1&_limit=20'
+
+
+
+async function getAllPosts() {
+  const posts = await fetch(url)
+  .then((res) => res.json());  return posts
+ 
+}
+
+const postsDiv = document.querySelector('.posts-div')
+
+async function criarDivPostagens() {
+  const criarDivComFetch = await getAllPosts();
+
+  criarDivComFetch.forEach(post => {
+    const div = document.createElement('div');
+    div.id = post.id;
+    div.className = 'postDiv'
+    div.innerHTML = 
+    `<p>Postagem número ${post.id}<p>
+    <div class="title">
+      <h2>${post.title}</h2>
+    </div>
+    <div class="post-body">
+      <p>${post.body}</p>
+    </div>
+    <div class="user-id"> Usuário ${post.userId}</div>`
+    
+
+    postsDiv.appendChild(div)
+  });
+  
+}
+
+criarDivPostagens();
+
