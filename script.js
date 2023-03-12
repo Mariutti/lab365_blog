a0f6c8862f9eb41be7a5a9b67d662286b43fe634
Pedro Alencar <pedromariutti@gmail.com>
2023-03-12 13:05:47 -0300

    Add button functionalities

--- script.js
+++ script.js
@@ -1,40 +1,53 @@
+const postsDiv = document.querySelector(".posts-div");
+const postPage = 1;
+const postsNumber = 20;
+let postPageCounter = postPage;
+let postNumberCounter = postsNumber;
 
-const url = 'https://jsonplaceholder.typicode.com/posts?_page=1&_limit=20'
+const btnGetPosts = document.querySelector("#getPosts");
 
+const getAllPosts = async function getAllPosts(url) {
+  const posts = await fetch(url).then((res) => res.json());
+  console.log(posts);
+  console.log(posts.length);
+  return posts;
+};
 
-
-async function getAllPosts() {
-  const posts = await fetch(url)
-  .then((res) => res.json());  
-  console.log(posts)
-  return posts
- 
-}
-
-const postsDiv = document.querySelector('.posts-div')
-
-async function criarDivPostagens() {
-  const criarDivComFetch = await getAllPosts();
-
-  criarDivComFetch.forEach(post => {
-    const div = document.createElement('div');
+async function criarDivPostagens(postPage, postsNumber) {
+  const url = `https://jsonplaceholder.typicode.com/posts?_page=${postPage}&_limit=${postsNumber}`;
+  const criarDivComFetch = await getAllPosts(url);
+  criarDivComFetch.forEach((post) => {
+    const div = document.createElement("div");
     div.id = post.id;
-    div.className = 'postDiv'
-    div.innerHTML = 
-    `<p>Postagem número ${post.id}<p>
+    div.className = "postDiv";
+    div.innerHTML = `<p>postagem ${post.id}<p>
     <div class="title">
-      <h2>${post.title}</h2>
+    <h2>${post.title}</h2>
     </div>
     <div class="post-body">
-      <p>${post.body}</p>
-    </div>
-    <div class="user-id"><p>Usuário ${post.userId}</p></div>`
-    
+    <p>${post.body}</p>
+    </div>`;
+    // <div class="user-id"><p>Usuário ${post.userId}</p></div>
 
-    postsDiv.appendChild(div)
+    postsDiv.appendChild(div);
   });
-  
 }
 
-criarDivPostagens();
+criarDivPostagens(postPage, postsNumber);
 
+btnGetPosts.addEventListener("click", getMorePosts);
+
+async function getMorePosts() {
+  postNumberCounter += postsNumber;
+  postPageCounter += postPage;
+  await criarDivPostagens(postPageCounter, postsNumber);
+
+  const postsDivCounter = document.querySelectorAll(".postDiv");
+
+  if(postsDivCounter.length < postNumberCounter){
+    btnGetPosts.style.display = 'none'
+    const p = document.createElement('p')
+    p.innerText = 'Não há mais postagens para carregar!'
+    postsDiv.appendChild(p)
+  }
+}
